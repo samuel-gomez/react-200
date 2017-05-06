89594afd828f3e9645329d5a735bc771922a4b35
Wolfgang Goedel <wgoedel@gmail.com>
2017-05-06 11:56:14 +0200

    implement reducer (naive approach)

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,5 +1,25 @@
-const initialState = {};
+const initialState = {
+  people: []
+};
 
-const reducer = (state = initialState, action) => state;
+const replaceOrInsert = (person, people) => {
+  const personIndex = people.findIndex(p => p.id === person.id);
+  if (personIndex >= 0) {
+    return [...people.slice(0, personIndex), person, ...people.slice(personIndex + 1)];
+  } else {
+    return [person, ...people];
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'PEOPLE_RECEIVED':
+      return { people: action.people };
+    case 'PERSON_RECEIVED':
+      return { people: replaceOrInsert(action.person, state.people) };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
